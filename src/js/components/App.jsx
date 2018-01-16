dfe1d61ac34cb3fdbcd35ea50ef294c7f7e6b59b
Vjeko <vjeko@E-Users-iMac.lan>
2018-01-16 13:55:48 +0100

    filtering items

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -20,12 +20,16 @@ const list = [
   }
 ]
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
 class App extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      list
+      list,
+      searchTerm: ''
     }
   }
 
@@ -39,26 +43,41 @@ class App extends Component {
     })
   }
 
+  onSearchChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
   render () {
+    const { searchTerm, list } = this.state
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
         {
-          this.state.list.map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <button
-                type="button"
-                onClick={() => this.onDismiss(item.objectID)}
-              >
-                Dismiss
-              </button>
-            </div>
-          )
+          list
+            .filter(isSearched(searchTerm))
+            .map(item =>
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <button
+                  type="button"
+                  onClick={() => this.onDismiss(item.objectID)}
+                >
+                  Dismiss
+                </button>
+              </div>
+            )
         }
       </div>
     )
