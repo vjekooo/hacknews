f03a8eb7f40c787814ef621784d0226429aa9560
Vjeko <vjeko@E-Users-iMac.lan>
2018-01-18 16:23:20 +0100

    added flow to project

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -1,11 +1,13 @@
 
+// @flow
+
 module.exports = {
   DEFAULT_QUERY: 'react',
   PATH_BASE: 'https://hn.algolia.com/api/v1',
   PATH_SEARCH: '/search',
   PARAM_SEARCH: 'query=',
   PARAM_PAGE: 'page=',
-  fetchData: async function (searchTerm, page = 0) {
+  fetchData: async function (searchTerm: string, page: number = 0) {
     const response = await fetch(
       `${this.PATH_BASE}${this.PATH_SEARCH}?${this.PARAM_SEARCH}${searchTerm}&${this.PARAM_PAGE}${page}`
     )
